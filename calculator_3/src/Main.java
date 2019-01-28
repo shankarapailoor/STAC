5ba584965c3c1a2a5610f1216da4a2aca77ef749
Shankara Pailoor <shankarapailoor@gmail.com>
2019-01-28 14:24:05 -0800

    calulator3: updating jar and code

--- calculator_3/src/Main.java
+++ calculator_3/src/Main.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner newInput = new Scanner(System.in);
         Calc c = new Calc();
-        while(newInput.hasNext()) {
-            String expression = newInput.nextLine();
-            long cost = c.computeExpression(expression);
-            System.out.println(cost);
-        }
+        long cost = c.computeExpression(expression);
+        System.out.println(cost);
     }
 }
